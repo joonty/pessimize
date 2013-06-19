facb98a8fbc47a8f76a42e133114c977035b21f4
Jon Cairns <jon@joncairns.com>
2013-06-19 14:15:32 +0100

    Add another Gemfile.lock test and tidy up spec

--- spec/gemfile_lock_version_parser_spec.rb
+++ spec/gemfile_lock_version_parser_spec.rb
@@ -4,15 +4,9 @@ require 'pessimize/gemfile_lock_version_parser'
 module Pessimize
 
   describe GemfileLockVersionParser do
-    context "with an example lock file" do
-      let(:lock_file) { data_file('Gemfile.lock.example') }
+    shared_examples "a gemfile lock parser" do |lock_file, expected_versions|
+
       let(:parser) { GemfileLockVersionParser.new }
-      let(:gems) { { 'diff-lcs'           => '1.2.4',
-                     'rake'               => '10.0.4',
-                     'rspec'              => '2.13.0',
-                     'rspec-core'         => '2.13.1',
-                     'rspec-expectations' => '2.13.0',
-                     'rspec-mocks'        => '2.13.1' } }
 
       before do
         parser.call lock_file
@@ -20,9 +14,37 @@ module Pessimize
 
       subject { parser.versions }
 
-      its(:length) { should == 6 }
-      it { should == gems }
+      its "versions should match" do
+        expected_versions.each do |name, version|
+          subject[name].should == version
+        end
+      end
+    end
 
+    context "with an example lock file" do
+      it_behaves_like "a gemfile lock parser",
+        data_file('Gemfile.lock.example'),
+        { 'diff-lcs'           => '1.2.4',
+          'rake'               => '10.0.4',
+          'rspec'              => '2.13.0',
+          'rspec-core'         => '2.13.1',
+          'rspec-expectations' => '2.13.0',
+          'rspec-mocks'        => '2.13.1' }
+    end
+
+    context "with a larger example lock file" do
+      it_behaves_like "a gemfile lock parser",
+        data_file('Gemfile.lock.example2'),
+        { 'actionmailer'         => '3.2.13',
+          'activesupport'        => '3.2.13',
+          'arel'                 => '3.0.2',
+          'binman'               => '3.3.1',
+          'capistrano'           => '2.15.4',
+          'capybara'             => '2.0.3',
+          'childprocess'         => '0.3.9',
+          'climate_control'      => '0.0.3',
+          'ci_reporter'          => '1.6.3',
+          'rails'                => '3.2.13' }
     end
   end
 end
