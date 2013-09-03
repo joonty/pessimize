ad00180f6c310cd9714a213ba2ec17c9b4b7cd0d
Jon Cairns <jon@joncairns.com>
2013-09-03 13:52:02 +0100

    Add constraint level to version mapper

--- spec/version_mapper_spec.rb
+++ spec/version_mapper_spec.rb
@@ -4,13 +4,49 @@ require 'pessimize/gem'
 
 module Pessimize
   describe VersionMapper do
-    context "with a gem and version hash" do
+    context "with a gem, version hash and minor constraint" do
       let(:gems)     { [ Gem.new('example') ] }
       let(:versions) { { 'example' => '2.2.3' } }
       let(:mapper)   { VersionMapper.new }
 
       before do
-        mapper.call gems, versions
+        mapper.call gems, versions, 'minor'
+      end
+
+      subject { gems.first }
+
+      its(:version) { should == '~> 2.2' }
+    end
+
+    context "with multiple gems, version hash and minor constraint" do
+      let(:gems)     { [ Gem.new('example'), Gem.new('fish', '1.3.2') ] }
+      let(:versions) { { 'example' => '1.4.9', 'fish' => '2.3.0' } }
+      let(:mapper)   { VersionMapper.new }
+
+      before do
+        mapper.call gems, versions, 'minor'
+      end
+
+      context "the first gem" do
+        subject { gems.first }
+
+        its(:version) { should == '~> 1.4' }
+      end
+
+      context "the second gem" do
+        subject { gems[1] }
+
+        its(:version) { should == '~> 2.3' }
+      end
+    end
+
+    context "with a gem, version hash and patch constraint" do
+      let(:gems)     { [ Gem.new('example') ] }
+      let(:versions) { { 'example' => '2.2.3' } }
+      let(:mapper)   { VersionMapper.new }
+
+      before do
+        mapper.call gems, versions, 'patch'
       end
 
       subject { gems.first }
@@ -18,13 +54,13 @@ module Pessimize
       its(:version) { should == '~> 2.2.3' }
     end
 
-    context "with multiple gems and version hash" do
+    context "with multiple gems, version hash and patch constraint" do
       let(:gems)     { [ Gem.new('example'), Gem.new('fish', '1.3.2') ] }
       let(:versions) { { 'example' => '1.4.9', 'fish' => '2.3.0' } }
       let(:mapper)   { VersionMapper.new }
 
       before do
-        mapper.call gems, versions
+        mapper.call gems, versions, 'patch'
       end
 
       context "the first gem" do
